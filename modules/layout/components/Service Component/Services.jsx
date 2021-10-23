79d251444ba49c249431e5bda5a0fc707345ba43
naoui mohamed <naoui.medcoc20@gmail.com>
2021-10-23 16:29:42 +0100

    feat: add service header (#17)

---
+++ modules/layout/components/Service Component/Services.jsx
@@ -0,0 +1,12 @@
+import DesktopDisplay from "./desktop/DesktopDisplay";
+
+const Services = () => {
+  return (
+    <>
+      <DesktopDisplay />
+      {/* later i'll add a mobile display */}
+    </>
+  );
+};
+
+export default Services;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { Box } from "@chakra-ui/layout";
 import Layout from "../modules/layout";
 import PostHeader from "../modules/layout/components/PostHeader/PostHeader";
-
+import Services from "../modules/layout/components/Service Component/Services";
 export default function Home() {
   return (
     <Layout>
@@ -9,6 +9,7 @@ export default function Home() {
         p={["20px 16px", "20px 30px", "20px 45px", "20px 100px", "20px 140px"]}
       >
         <PostHeader />
+        <Services />
       </Box>
     </Layout>
   );
