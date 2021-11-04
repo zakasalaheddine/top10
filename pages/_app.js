42b33f16a59ae0401126ce72131c2e27a0ab8a82
naoui mohamed <naoui.medcoc20@gmail.com>
2021-11-04 11:36:57 +0100

    feat: install packages (#53)

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import { theme } from '../utils/theme'
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../utils/theme";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
