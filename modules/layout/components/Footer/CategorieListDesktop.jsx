7246942bd38ef979c9ec3315533855e6c911fdcb
naoui mohamed <naoui.medcoc20@gmail.com>
2021-11-04 11:55:40 +0100

    feat: add slider to footer categories (#55)

--- modules/layout/components/Footer/CategorieListDesktop.jsx
+++ modules/layout/components/Footer/CategorieListDesktop.jsx
@@ -1,28 +1,29 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { Center, HStack, Text } from "@chakra-ui/layout";
+import Slider from "react-slick";
+import { Center, Flex, Text } from "@chakra-ui/layout";
 import Categorie from "./Categorie";
+import SliderSettings from "./SliderSettings";
 const CategorieList = ({ categories }) => {
   console.log(categories);
   return (
-    <CategoriesContainer spacing={5} display={["none", "none", "flex"]}>
-      <CategorieContainer w="64px" bgColor="primary">
+    <Flex spacing={5} display={["none", "none", "flex"]}>
+      <CategorieContainer w="64px" mr="10px" bgColor="primary">
         <Text fontSize="xl" fontWeight="bold" color="#fff">
           All
         </Text>
       </CategorieContainer>
-      {categories.map((categorie, i) => (
-        <Categorie key={i} categorie={categorie} />
-      ))}
-    </CategoriesContainer>
+      <Slider {...SliderSettings}>
+        {categories.map((categorie, i) => (
+          <Categorie key={i} categorie={categorie} />
+        ))}
+      </Slider>
+    </Flex>
   );
 };
 
 export default CategorieList;
 
-const CategoriesContainer = styled(HStack)`
-  overflow: auto;
-`;
 export const CategorieContainer = styled(Center)`
   flex-shrink: 0;
   border-radius: 15px;
